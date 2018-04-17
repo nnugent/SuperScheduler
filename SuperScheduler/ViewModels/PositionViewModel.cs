730d52a12f8b88660a815d7dfed799b06948445b
nnugent <nrnugent96@gmail.com>
2018-04-17 14:30:14 -0500

    Added viewmodels and views for all positions and all shiftstarttimes

---
+++ SuperScheduler/ViewModels/PositionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperScheduler.ViewModels
+{
+    public class PositionViewModel
+    {
+        public List<String> Positions { get; set; }
+        
+    }
+}
---
+++ SuperScheduler/ViewModels/ShiftStartTimesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperScheduler.ViewModels
+{
+    public class ShiftStartTimesViewModel
+    {
+        public List<TimeSpan> ShiftStartTimes { get; set; }
+
+    }
+}
